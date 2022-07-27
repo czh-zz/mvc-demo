eb0ecbfaa24c70b1fd849691ad10a340eadc6271
chenzh <515740090@qq.com>
2022-07-27 22:02:52 +0800

    添加数据保存，初步完成mvc

--- src/app2.js
+++ src/app2.js
@@ -1,14 +1,34 @@
 import './app2.css'
 import $ from 'jquery'
 
+const html = `
+        <section id="app2">
+            <ol class="tab-bar">
+                <li>1</li>
+                <li>2</li>
+            </ol>
+            <ul class="tab-content">
+                <li>内容1</li>
+                <li>内容2</li>
+            </ul>
+        </section>
+`
+const $element = $(html).appendTo($('body >.page'))
+
 const $tabBar = $('#app2 .tab-bar')
 const $tabContent = $('#app2 .tab-content')
+const localKey = 'app2.index'
+const index = localStorage.getItem(localKey) || 0
+
 //监听li元素事件，并获取第几个li
 $tabBar.on('click', 'li', (e) => {
     const $li = $(e.currentTarget);
     $li.addClass("selected")
         .siblings().removeClass("selected")
     const index = $li.index()
+    //离开页面时，保留当前页面的变动
+    localStorage.setItem(localKey, index)
+
     $tabContent.children()
         //第index个显示
         .eq(index).addClass("active")
@@ -22,4 +42,4 @@ $tabBar.on('click', 'li', (e) => {
     .siblings().css({ display: 'none' })*/
 });
 //默认显示，触发事件（也可在index页面的li分别加elected，active）
-$tabBar.children().eq(0).trigger('click')
+$tabBar.children().eq(index).trigger('click')
--- src/app3.js
+++ src/app3.js
@@ -1,8 +1,35 @@
 import './app3.css'
 import $ from 'jquery'
 
+const html = `
+        <section id="app3">
+            <div class="square"></div>
+        </section>
+`
+
+const $element = $(html).appendTo($('body>.page'))
+
 const $square = $('#app3 .square')
+const localKey = 'app3.active'
+const active = localStorage.getItem(localKey) === 'yes' ? true : false
+/*
+if(active){
+    $square.addClass('active')
+}else{
+    $square.removeClass('active')
+}*/
+//简写成
+$square.toggleClass('active', active)
+
+
 $square.on('click', () => {
     //toggleClass如果没就加上，如果有就删除
-    $square.toggleClass('active')
+    //$square.toggleClass('active')等价于如下
+    if ($square.hasClass('active')) {
+        $square.removeClass('active')
+        localStorage.setItem(localKey, 'no')
+    } else {
+        $square.addClass('active')
+        localStorage.setItem(localKey, 'yes')
+    }
 })
